1eeef64e6bc793ab9221a8bb3524c7fe99017d7d
Mary Vokicic <mvoki@esailors.de>
2016-08-16 19:29:36 +0000

    added from handling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,63 @@
 function App() {
-  this.form = document.querySelector('#contactForm');
+  this.form = document.forms.contactForm;
+  this.form.noValidate = true;
   this.addSubmitListener();
 }
 
-App.prototype.addSubmitListener = function() {
+App.prototype.addSubmitListener = function () {
   var scope = this;
-  this.form.onsubmit = function(event) {
+
+  this.form.onsubmit = function (event) {
     event.preventDefault();
-    return scope.validate(event.target);
+    if (scope.validate(this)) {
+      this.submit();
+    }
   };
+
 };
 
-App.prototype.validate= function(form) {
-  // Here you could handle your own form validation
-  // know it is done by the browser defaults.
+App.prototype.validate = function (form) {
+  var isValid = true;
 
-  alert('Form cannot be submited - Sorry, it is not our working day...');
-  return false;
+  for (var i = 0; i < form.length; i++) {
+    if (!form[i].checkValidity()) {
+      isValid = false;
+      this.showError(form[i]);
+    } else {
+      this.hideError(form[i]);
+    }
+  }
+  return isValid;
 };
+
+App.prototype.showError = function(formField) {
+  var classes = formField.className.split(' '),
+      errClass = classes[0] + '--invalid',
+      errMsgElement = formField.parentNode.querySelector('.form__input-block-msg');
+
+  if (classes.indexOf(errClass) < 0) {
+    classes.push(errClass);
+    formField.className = classes.join(' ');
+  }
+
+  if (errMsgElement) {
+    errMsgElement.style = 'display:block';
+  }
+};
+
+App.prototype.hideError = function(formField) {
+  var classes = formField.className.split(' '),
+      errClass = classes[0] + '--invalid',
+      errMsgElement = formField.parentNode.querySelector('.form__input-block-msg');
+
+  if (classes.indexOf(errClass) >= 0) {
+    formField.className = classes.filter(function(cl) {
+      return cl !== errClass;
+    }).join(' ');
+  }
+
+  if (errMsgElement) {
+    errMsgElement.style = '';
+  }
+};
+
